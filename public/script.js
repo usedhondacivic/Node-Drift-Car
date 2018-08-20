43b46c4d24fcc6f8cd6c750f0d9c4da4420128db
Michael Crum <michaelcrum@Michaels-MacBook-Pro.local>
2018-08-20 08:33:58 -0400

    Impliment car + working on changes to drift physics

--- public/script.js
+++ public/script.js
@@ -28,13 +28,48 @@ socket.on("state", function(items){
 });
 
 var renderPlayer = function(instance) {
-    noStroke();
+    /*noStroke();
     fill(0, 0, 0);
-    rect(instance.x,instance.y,instance.width,instance.height);
+    rect(instance.x,instance.y,instance.width,instance.height);*/
+    /*if(false){
+        instance.wheelTrails[0].push([instance.pos.x + 2*Math.sin(-instance.dir) + -12*cos(-instance.dir), instance.pos.y + 2*Math.cos(-instance.dir) + 12*Math.sin(-instance.dir), 50, instance.dir]);
+        instance.wheelTrails[1].push([instance.pos.x + -2*Math.sin(-instance.dir) + -12*cos(-instance.dir), instance.pos.y + -2*Math.cos(-instance.dir) + 12*Math.sin(-instance.dir), 50, instance.dir]);
+        instance.wheelTrails[2].push([instance.pos.x + 2*Math.sin(-instance.dir) + 2*cos(-instance.dir), instance.pos.y + 2*Math.cos(-instance.dir) - 2*Math.sin(-instance.dir), 50, instance.dir]);
+        instance.wheelTrails[3].push([instance.pos.x + -2*Math.sin(-instance.dir) + 2*cos(-instance.dir), instance.pos.y + -2*Math.cos(-instance.dir) - 2*Math.sin(-instance.dir), 50, instance.dir]);
+    }
+    if(instance.wheelTrails[0].length>0){
+        line(instance.wheelTrails[0][instance.wheelTrails[0].length-1][0], instance.wheelTrails[0][instance.wheelTrails[0].length-1][1], instance.pos.x + 5*Math.sin(-instance.dir) + -5*cos(-instance.dir), instance.pos.y + 5*Math.cos(-instance.dir) + 5*Math.sin(-instance.dir));
+        line(instance.wheelTrails[1][instance.wheelTrails[1].length-1][0], instance.wheelTrails[1][instance.wheelTrails[1].length-1][1], instance.pos.x + 5*Math.sin(-instance.dir) + -5*cos(-instance.dir), instance.pos.y + 5*Math.cos(-instance.dir) + 5*Math.sin(-instance.dir));
+    }
+    for(var i in instance.wheelTrails){
+        for(var o=0; o<instance.wheelTrails[i].length; o++){
+            instance.wheelTrails[i][o][2]--;
+            if(instance.wheelTrails[i][o][2]<0){
+                instance.wheelTrails[i].splice(o,1);
+            }
+            if(o+1 < instance.wheelTrails[i].length){
+                noStroke();
+                fill(0, instance.wheelTrails[i][o][2]*5);
+                pushMatrix();
+                translate(instance.wheelTrails[i][o][0], instance.wheelTrails[i][o][1]);
+                rotate(instance.dir);
+                //rect(-2.5, -1, 5, 2);
+                noStroke();
+                popMatrix();
+            }
+        }
+    }*/
+    noStroke();
+    fill(0, 255, 255);
+    push();
+        translate(instance.pos.x,instance.pos.y);
+        rotate(instance.dir);
+        rect(-15,-5,20,10);
+    pop();
 }
 
 var renderBullet = function(instance) {
     noStroke();
     fill(255,0,0);
-    rect(instance.x,instance.y,3,3);
+    //rect(instance.x,instance.y,3,3);
 }
--- server.js
+++ server.js
@@ -35,7 +35,6 @@ io.on("connection", function (socket) {
         var currentPlayer = toSend["players"][socket.id] || {x:0, y:0};
         var angle = Math.atan2(arg.y-currentPlayer.y, arg.x-currentPlayer.x);
         toSend["bullets"].push(new bullet(currentPlayer.x, currentPlayer.y, Math.cos(angle)*currentPlayer.shotSpeed, Math.sin(angle)*currentPlayer.shotSpeed, socket.id));
-        console.log(currentPlayer.x);
     });
 
     socket.on("disconnect", function(){
@@ -64,7 +63,7 @@ var RIGHT_ARROW = 39;
 var UP_ARROW = 38;
 var DOWN_ARROW = 40;
 
-var player=function(x,y){
+/*var player=function(x,y){
     this.x=x;
     this.y=y;
     this.width=5;
@@ -84,7 +83,77 @@ var player=function(x,y){
         this.x += this.velocityX;
         this.y += this.velocityY;
     }
-}
+}*/
+/*
+var player=function(x,y){
+    this.pos=new Vector(x, y);
+    this.vel=new Vector(0,0);
+    this.x=200;
+    this.y=200;
+    this.friction=0.96;
+    this.speedFriction=0.85;
+    this.speed=0;
+    this.accel=0.05;
+    this.dir=0;
+    this.turnAdjust = 0.03;
+    this.turnSpeed=0.7;
+    this.turnDamp=70;
+    this.wheelTrails=[[],[],[],[]];
+    this.keys=[];
+    this.draw=function(){
+        this.update();
+    };
+    this.update=function(){
+        this.pos.addTo(this.vel);
+        //console.log(new Vector(Math.cos(this.dir)*this.speed,Math.sin(this.dir)*this.speed));
+        this.vel.addTo(new Vector(Math.cos(this.dir)*this.speed,Math.sin(this.dir)*this.speed));
+        this.vel.multiplyBy(this.friction);
+        this.speed*=this.speedFriction;
+        if(this.keys[UP_ARROW]){this.speed+=this.accel;}else{
+            this.vel.addTo(new Vector(Math.cos(this.dir)*this.vel.getMagnitude()*this.turnAdjust,Math.sin(this.dir)*this.vel.getMagnitude()*this.turnAdjust));
+        }
+        if(this.keys[DOWN_ARROW]){this.speed-=this.accel*0.5;}
+        if(this.keys[LEFT_ARROW]){this.dir-=(this.turnSpeed*this.vel.getMagnitude())/this.turnDamp;}
+        if(this.keys[RIGHT_ARROW]){this.dir+=(this.turnSpeed*this.vel.getMagnitude())/this.turnDamp;}
+    };
+};
+*/
+var player=function(x,y){
+    this.pos=new Vector(x, y);
+    this.vel=new Vector(0,0);
+    this.x=200;
+    this.y=200;
+    this.sideFriction=1.00;
+    this.forwardFriction=1.00;
+    this.speed=0;
+    this.accel=0.05;
+    this.decl=0.90;
+    this.dir=0;
+    this.turnSpeed=0.7;
+    this.turnDamp=70;
+    this.wheelTrails=[[],[],[],[]];
+    this.keys=[];
+    this.draw=function(){
+        this.update();
+    };
+    this.update=function(){
+        this.pos.addTo(this.vel);
+        this.vel.addTo(new Vector(Math.cos(this.dir)*this.speed,Math.sin(this.dir)*this.speed));
+        var sideVector = new Vector(this.sideFriction, 0);
+        sideVector.setDirection(this.dir-Math.PI/2);
+        //sideVector.multiplyScalar(this.sideFriction);
+        //this.vel.multiplyBy(sideVector);
+        var frontVector = new Vector(this.forwardFriction, 0);
+        frontVector.setDirection(this.dir);
+        this.vel.multiplyBy(frontVector);
+        console.log(this.dir);
+        this.speed*=this.decl;
+        if(this.keys[UP_ARROW]){this.speed+=this.accel;}
+        if(this.keys[DOWN_ARROW]){this.speed-=this.accel*0.5;}
+        if(this.keys[LEFT_ARROW]){this.dir-=(this.turnSpeed*this.vel.getMagnitude())/this.turnDamp;}
+        if(this.keys[RIGHT_ARROW]){this.dir+=(this.turnSpeed*this.vel.getMagnitude())/this.turnDamp;}
+    };
+};
 
 var bullet=function(x,y,vx,vy, owner){
     this.x=x;
@@ -97,3 +166,102 @@ var bullet=function(x,y,vx,vy, owner){
         this.y+=this.vy;
     }
 }
+
+var Vector = function(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+  };
+  
+  // return the angle of the vector in radians
+  Vector.prototype.getDirection = function() {
+      return Math.atan2(this.y, this.x);
+  };
+  
+  // set the direction of the vector in radians
+  Vector.prototype.setDirection = function(direction) {
+      var magnitude = this.getMagnitude();
+    this.x = Math.cos(direction) * magnitude;
+    this.y = Math.sin(direction) * magnitude;
+  };
+  
+  // get the magnitude of the vector
+  Vector.prototype.getMagnitude = function() {
+      // use pythagoras theorem to work out the magnitude of the vector
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+  };
+  
+  // set the magnitude of the vector
+  Vector.prototype.setMagnitude = function(magnitude) {
+      var direction = this.getDirection(); 
+      this.x = Math.cos(direction) * magnitude;
+      this.y = Math.sin(direction) * magnitude;
+  };
+  
+  // add two vectors together and return a new one
+  Vector.prototype.add = function(v2) {
+      return new Vector(this.x + v2.x, this.y + v2.y);
+  };
+  
+  // add a vector to this one
+  Vector.prototype.addTo = function(v2) {
+      this.x += v2.x;
+    this.y += v2.y;
+  };
+  
+  // subtract two vectors and reutn a new one
+  Vector.prototype.subtract = function(v2) {
+      return new Vector(this.x - v2.x, this.y - v2.y);
+  };
+  
+  // subtract a vector from this one
+  Vector.prototype.subtractFrom = function(v2) {
+      this.x -= v2.x;
+    this.y -= v2.y;
+  };
+  
+  // multiply this vector by a scalar and return a new one
+  Vector.prototype.multiplyScalar = function(scalar) {
+    this.x *= scalar;
+    this.y *= scalar;
+  };
+  
+  // multiply this vector by the scalar
+  Vector.prototype.multiplyBy = function(scalar) {
+    this.x *= scalar.x;
+    this.y *= scalar.y;
+  };
+  
+  // scale this vector by scalar and return a new vector
+  Vector.prototype.divide = function(scalar) {
+    return new Vector(this.x / scalar, this.y / scalar);
+  };
+  
+  // scale this vector by scalar
+  Vector.prototype.divideBy = function(scalar) {
+    this.x /= scalar;
+    this.y /= scalar;
+  };
+  
+  // Aliases
+  Vector.prototype.getLength = Vector.prototype.getMagnitude;
+  Vector.prototype.setLength = Vector.prototype.setMagnitude;
+  
+  Vector.prototype.getAngle = Vector.prototype.getDirection;
+  Vector.prototype.setAngle = Vector.prototype.setDirection;
+  
+  // Utilities
+  Vector.prototype.copy = function() {
+    return new Vector(this.x, this.y);
+  };
+  
+  Vector.prototype.toString = function() {
+    return 'x: ' + this.x + ', y: ' + this.y;
+  };
+  
+  Vector.prototype.toArray = function() {
+    return [this.x, this.y];
+  };
+  
+  Vector.prototype.toObject = function() {
+    return {x: this.x, y: this.y};
+  };
