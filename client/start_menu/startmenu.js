2685b8a1c4289f277c56c23089fba12791493b5f
Michael Crum <micacer48@gmail.com>
2018-11-22 21:55:17 -0700

    Added join buttons to server list Added back button for menu nav Removed quick start, redirects to server page

--- client/start_menu/startmenu.js
+++ client/start_menu/startmenu.js
@@ -5,6 +5,8 @@ var textBox;
 var roomBox;
 var serverList;
 
+var pageHistory = [];
+
 window.onload = function(){
     sessionStorage.setItem('setup', "false");
     textBox = document.getElementById("nickname");
@@ -26,34 +28,73 @@ window.onload = function(){
 }
 
 socket.on("rooms", function(data){
-    serverList.innerHTML = "";
+    if(!serverList){
+        return;
+    }
+    serverList.innerHTML = "<tr><th>Server name</th><th>Players</th><th>Track</th></tr>";
     for(var i in data){
         var d = data[i];
-        var newLine = document.createElement("LI");
-        var text = document.createTextNode(d.name+"     "+d.currentPlayers+"/"+d.maxPlayers+"      "+d.track);
-        newLine.appendChild(text);
-        serverList.appendChild(newLine);
+        var newRow = document.createElement("TR");
+        var name = document.createElement("TD");
+        name.innerHTML = d.name;
+        var players = document.createElement("TD");
+        players.innerHTML = d.currentPlayers+"/"+d.maxPlayers;
+        var track = document.createElement("TD");
+        track.innerHTML = d.track;
+        var join = document.createElement("input");
+        join.setAttribute("type", "button");
+        join.setAttribute("onclick", "submit('"+d.name+"')");
+        join.setAttribute("class", "smallButton");
+        join.setAttribute("value","Join");
+        track.appendChild(join);
+        newRow.appendChild(name);
+        newRow.appendChild(players);
+        newRow.appendChild(track);
+        serverList.childNodes[0].appendChild(newRow);
     }
 });
 
-function submit() {
+function start() {
     if(textBox){
         if(textBox.value.length > 0){
-            sessionStorage.setItem('nickname', textBox.value);
-            sessionStorage.setItem('setup', "true");
-            if(roomBox.value.length > 0){
-                window.location.replace("./race/"+roomBox.value);
-            }else{
-                window.location.replace("./race/default"+roomBox.value);
-            }
+            switchPage('serverPage');
         }else{
             document.getElementById("warning").style.display = "block";
         }
     }
-}
+};
+
+function submit(room) {
+    if(textBox){
+        if(textBox.value.length > 0){
+            sessionStorage.setItem('nickname', textBox.value);
+            sessionStorage.setItem('setup', "true");
+            if(room.length > 0){
+                window.location.replace("./race/"+room);
+            }else{
+                window.location.replace("./race/default");
+            }
+        }else{
+            alert("Hey, what do you think your tryna do buddy.");
+        }
+    }
+};
 
 function switchPage(newPage) {
     document.getElementById(currentPage).style.display = "none";
+    pageHistory.push(currentPage);
     currentPage = newPage;
+    document.getElementById(currentPage).style.display = "inline-block";
+    document.getElementById("backButton").style.display = "inline-block";
+};
+
+function backPage() {
+    console.log("hit");
+    document.getElementById(currentPage).style.display = "none";
+    currentPage = pageHistory[pageHistory.length-1];
+    pageHistory.pop();
+    if(pageHistory.length == 0){
+        document.getElementById("backButton").style.display = "none";
+    }
     document.getElementById(currentPage).style.display = "inline-block";
 }
