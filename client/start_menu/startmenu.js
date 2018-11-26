d6b80bb36a380fe431b88fdaa32b9f5952ad2e98
Crum <micacer48@gmail.com>
2018-11-26 13:50:35 -0700

    Work on car selection

--- client/start_menu/startmenu.js
+++ client/start_menu/startmenu.js
@@ -44,11 +44,7 @@ window.onload = function(){
         }
     });
     
-    var cars = document.getElementsByClassName("carMaskImage");
-    for(var i in cars){
-        var tinto = new Tinto(cars[i]);
-        cars[i].src = tinto.imageDataWithTintColor("#f44242");
-    }
+    updateCars();
 }
 
 socket.on("rooms", function(data){
@@ -143,6 +139,16 @@ function cycleTrack(dir){
     document.getElementById("trackName").innerHTML = circuitWrappers[trackSelectIndex].name;
 };
 
+function updateCars(){
+    var cars = document.getElementsByClassName("carMaskImage");
+    var slider = document.getElementById("colorPicker");
+    for(var i in cars){
+        var tinto = new Tinto(cars[i]);
+        console.log(hslToHex(slider.value, 40, 100));
+        cars[i].src = tinto.imageDataWithTintColor(hslToHex(slider.value, 100, 80));
+    }
+}
+
 function loadJSON(src, callback) {   
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
@@ -155,3 +161,32 @@ function loadJSON(src, callback) {
     };
     xobj.send(null);  
  };
+
+ function hslToHex(h, s, l) {
+    h /= 100;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
