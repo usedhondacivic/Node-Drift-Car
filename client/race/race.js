48ab0f50d24d62cf3ba317a8aff03c9b018bc048
Crum <micacer48@gmail.com>
2019-01-29 09:14:38 -0700

    Fixed spacebar in chat bug

--- client/race/race.js
+++ client/race/race.js
@@ -38,7 +38,7 @@ window.onload = function(){
     });
     window.addEventListener("keydown", function(e) {
         // space and arrow keys
-        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
             e.preventDefault();
         }
     }, false);
